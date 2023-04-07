4170b8e2cb47f9ca6513c47379cd5a54db348267
Juan <juan_jg@hotmail.com>
2023-04-07 05:02:14 -0300

    updated .gitignore

--- Deitel_CH07_Pointers/D7_ejm02_argByReferenceToFunction.c
+++ Deitel_CH07_Pointers/D7_ejm02_argByReferenceToFunction.c
@@ -1,42 +1,37 @@
-/*
-    Programa : D7_ejm02_argByReferenceToFunction.c
-    Propósito: Escribir un programa que muestre la diferencia de pasar argumentos a una
-               función por valor y por referencia con el uso de apuntadores
-    Fecha    : 15/08/2019
- */
-
 #include <stdio.h>
 
-int cuboPorValor(int n);
-void cuboPorReferencia(int *ptrN);
+int cubeByValue(int n);
+void cubeByReference(int *nPtr);
 
-int main()
+int main(void)
 {
-    int num1 = 5, num2 = 7;
+    int number = 5;
+    int num;
 
-    printf("===================================================\n");
-    printf("\tLlamadas por valor y por referencia\n");
-    printf("===================================================\n");
+    printf("========================================================\n");
+    printf("\tPassing Arguments value and by reference\n");
+    printf("========================================================\n");
+    printf("The original value of number is %d\n\n", number);
 
-    printf("El valor original de numero 1 es: %d\n", num1);
-    printf("El valor original de numero 2 es: %d\n", num2);
+    // pass number by value to cubeByValue
+    num = cubeByValue(number);
+    printf("Number by value: %d\n", num);
 
-    num1 = cuboPorValor(num1); // Pasa num1 por valor a la funcion cuboPorValor()
-    cuboPorReferencia(&num2);  // Pasa la dirección de num2 a cuboPorReferencia()
+    printf("\nValue of number: %d\n\n", number);
 
-    printf("El nuevo valor de numero 1 es: %d\n", num1);
-    printf("El nuevo valor de numero 2 es: %d\n", num2);
-    printf("===================================================\n");
+    // pass number by reference to cubeByReference
+    cubeByReference(&number);
+    printf("Number by reference: %d\n", number);
 
-    return 0;
+    puts("");
 }
 
-int cuboPorValor(int n)
+int cubeByValue(int n)
 {
     return n * n * n;
 }
 
-void cuboPorReferencia(int *ptrN) // El pasar un argumento como apuntador
-{                                 // modifica la variable en main
-    *ptrN = *ptrN * *ptrN * *ptrN;
+void cubeByReference(int *nPtr)
+{
+    *nPtr = *nPtr * *nPtr * *nPtr;
 }

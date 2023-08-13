880d778d1962e9d3376cc0c27b447ec2705ac407
Juan <juan_jg@hotmail.com>
2023-08-13 05:24:14 -0300

    added exercicio 2.33

---
+++ Deitel_CH02_IntroductionToCProgramming/D2_ejr2-33_CarPoolCalculator.c
@@ -0,0 +1,15 @@
+/* program: Car Pool Calculation
+   @brief : Research several car-pooling websites. Create an application that calculates your daily driving cost, so that you can estimate how much money could be saved by car pooling, which also has other advantages such as reducing carbon emissions and reducing traffic congestion. The application should input the following information and display the user’s cost per day of driving to work:
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  int milesPerDay, costPerGallon;
+
+  printf("### Card Pool Calculation \a\n");
+
+  printf("Total miles driven per day: \n");
+  printf("Cost per gallon of gasoline: \n");
+} // end function main

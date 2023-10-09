6a2ac391dad94956ff7493de82258f1694b1d119
Juan <juan_jg@hotmail.com>
2023-10-09 16:19:58 -0300

    updated example 8, cap 15 function templates

--- Deitel_CH15_CppAsaABetterC/D15_ejm08_plantillasDeFunciones.cpp
+++ Deitel_CH15_CppAsaABetterC/D15_ejm08_plantillasDeFunciones.cpp
@@ -1,54 +1,49 @@
 /*
  Programa : ejm_08_plantillasDeFunciones.cpp
  Proposito: Mostrar el uso de una plantilla de función
- Fecha    : 19/10/2018
  */
 
 #include <iostream>
 
-using std::cout;
-using std::cin;
-using std::endl;
+using namespace std;
 
-template < class T >                    // o template < typename T >
-T maximo(T valor1, T valor2, T valor3)
-{
-  T max = valor1;
+template <class M>  // o template < typename T >
+M maximum(M valor1, M valor2, M valor3) {
+   M max = valor1;
 
-  if(valor2 > max)
-    max = valor2;
+   if (valor2 > max) max = valor2;
 
-  if(valor3 > max)
-    max = valor3;
+   if (valor3 > max) max = valor3;
 
-  return max;
+   return max;
 }
 
-int main()
-{
-  int int1, int2, int3;
+int main() {
+   int int1, int2, int3;
 
-  cout << "==============================================" << endl;
-  cout << "\tPlantillas de Funciones" << endl;
-  cout << "==============================================" << endl;
+   cout << "==============================================" << endl;
+   cout << "\tPlantillas de Funciones" << endl;
+   cout << "==============================================" << endl;
 
-  cout << "Introduzca tres numeros enteros: ";                // version int
-  cin >> int1 >> int2 >> int3;
-  cout << "El valor entero maximo es: " << maximo(int1, int2, int3) << endl;
-  cout << "==============================================" << endl;
+   cout << "Introduzca tres numeros enteros: ";  // version int
+   cin >> int1 >> int2 >> int3;
+   cout << "El valor entero maximo es: " << maximum(int1, int2, int3) << endl;
+   cout << "==============================================" << endl;
 
-  double doble1, doble2, doble3;
+   double doble1, doble2, doble3;
 
-  cout << "Introduzca tres numeros double: ";                // version double
-  cin >> doble1 >> doble2 >> doble3;
-  cout << "El valor double maximo es: " << maximo(doble1, doble2, doble3) << endl; 
-  cout << "==============================================" << endl; 
+   cout << "Introduzca tres numeros double: ";  // version double
+   cin >> doble1 >> doble2 >> doble3;
+   cout << "El valor double maximo es: " << maximum(doble1, doble2, doble3)
+        << endl;
+   cout << "==============================================" << endl;
 
-  char char1, char2, char3;
-  cout << "Introduzca tres caracteres: ";                // version char
-  cin >> char1 >> char2 >> char3;
-  cout << "El valor caracter maximo es: " << maximo(char1, char2, char3) << endl;
-  cout << "==============================================" << endl; 
+   char char1, char2, char3;
+   cout << "Introduzca tres caracteres: ";  // version char
+   cin >> char1 >> char2 >> char3;
+   cout << "El valor caracter maximo es: " << maximum(char1, char2, char3)
+        << endl;
+   cout << "==============================================" << endl;
 
-  return 0;
+   return 0;
 }

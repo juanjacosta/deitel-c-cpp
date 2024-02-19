21e39c58a5be618891953ba015dc325591ff2885
Juan <juan_jg@hotmail.com>
2024-02-19 00:49:52 -0300

    updated example copy strings into arrays

--- Deitel_CH07_Pointers/D7_ejm12_copiarCadenasConApuntadores.c
+++ Deitel_CH07_Pointers/D7_ejm12_copiarCadenasConApuntadores.c
@@ -1,47 +1,55 @@
 /*
     Programa : D7_ejm12_copiarCadenasConApuntadores.c
-    Propósito: Escribir un programa que muestre el uso de la notacion de arreglos 
-               con apuntadores mediante dos funciones que copian uno a uno los 
-               elementos de un arreglo en otro.
+    Propósito: Escribir un programa que muestre el uso de la notación de
+               arreglos con apuntadores mediante dos funciones que copian uno a
+               uno los elementos de un arreglo en otro.
 
-               Ejm Deitel - Cap 7 - Ejm 7.20 - pg 255
-    Fecha    : 24/09/2019
+               Ejm Deitel - Cap 7 - Ejm 7.21 - pg 333
  */
 
 #include <stdio.h>
+#define SIZE 10
 
-void copia1(char *s1, const char *s2);  // prototipo 
-void copia2(char *s1, const char *s2);  // prototipo 
+void copy1 (char *const s1, const char *const s2); // prototipo
+void copy2 (char *s1, const char *s2);             // prototipo
 
-
-int main()
+int main ()
 {
-    char cadena1[10];           // crea el arreglo cadena1 
-    char *cadena2 = "Hola";     // crea un apuntador a una cadena 
-    char cadena3[10];           // crea el arreglo cadena3 
-    char cadena4[] = "Adios";   // crea un apuntador a una cadena 
+  char cadena1[SIZE];          // crea el arreglo cadena1
+  char *cadena2 = "Hello you"; // crea un apuntador a una cadena
 
-    copia1(cadena1, cadena2);
-    printf("cadena1 = %s\n", cadena1);
+  copy1 (cadena1, cadena2);
+  printf ("cadena1 = %s\n", cadena1);
 
-    copia2(cadena3, cadena4);
-    printf("cadena3 = %s\n", cadena3);
+  char cadena3[SIZE];          // crea el arreglo cadena3
+  char cadena4[] = "Good Bye"; // crea un array contendo una cadena
 
-    return 0;
+  copy2 (cadena3, cadena4);
+  printf ("cadena3 = %s\n", cadena3);
 }
 
-void copia1(char *s1, const char *s2)
+/*
+ *  @brief copy s2 to s1 using array notation
+ *  @param s1 recibe um ponteiro constante para um dado não constante
+ *  @param s2 recebe um ponteiro constante para um dado constante
+ */
+void copy1 (char *const s1, const char *const s2)
 {
-    int i;                                          // copia s2 en s1 con el uso 
-                                                    // de la notacion de arreglos 
-    for (i = 0; (s1[i] = s2[i]) != '\0'; i++){
-        ;                                           // No realiza tarea alguna en el cuerpo 
+  for (size_t i = 0; (s1[i] = s2[i]) != '\0'; i++)
+    {
+      ; // do nothing in body
     }
 }
 
-void copia2(char *s1, const char *s2)               // copia s2 en s1 con el uso de la 
-{                                                   // notacion de apuntadores 
-    for (; (*s1 = *s2) != '\0'; s1++, s2++){
-        ;                                           // No realiza tarea alguna en el cuerpo 
+/*
+ *  @brief copy s2 to s1 using pointer notation
+ *  @param s1 recibe um ponteiro não constante para um dado não constante
+ *  @param s2 recebe um ponteiro não constante para um dado constante
+ */
+void copy2 (char *s1, const char *s2) // copia s2 en s1 con el uso de la
+{                                     // notación de apuntadores
+  for (; (*s1 = *s2) != '\0'; s1++, s2++)
+    {
+      ; // do nothing in body
     }
 }
